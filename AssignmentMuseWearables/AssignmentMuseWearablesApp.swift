32f95f6c14b5fca96cf52f45e79e26ed62a2124a
MITHUN M R <mithunmr286@gmail.com>
2023-12-06 13:26:31 +0530

    Initial Commit

---
+++ AssignmentMuseWearables/AssignmentMuseWearablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentMuseWearablesApp.swift
+//  AssignmentMuseWearables
+//
+//  Created by MRM on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentMuseWearablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
